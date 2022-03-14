1bbc803bf1ad789d68ff8f23cc2f0a1889a122ad
Samuel Makinde <makindesamuel1999@gmail.com>
2022-03-14 03:51:20 -0700

    learning c variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - to print base 10 number
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}

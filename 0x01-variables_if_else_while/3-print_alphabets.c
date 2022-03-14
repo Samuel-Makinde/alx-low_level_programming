85268f93e5e31222d26ebd1792120a61ef9b7048
Samuel Makinde <makindesamuel1999@gmail.com>
2022-03-14 02:40:38 -0700

    learning c variables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - to print in both upper and lower case
+ *
+ * description: to print upper and lower case
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	       putchar(ch);
+	putchar('\n');
+	return (0);
+}

650a4e51f8ea4ed99cf0fcfe3ffa21e9c26b9467
Samuel Makinde <makindesamuel1999@gmail.com>
2022-03-15 17:01:01 -0700

    learning C function and nested loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/***
+ * print_alphabet - prints alphabet 10 times in lowercase.
+ * Description: to be followed by a new line
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 97; ch < 123; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n')
+	}
+}
